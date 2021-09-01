66a609c6c313c5a8f826a4b28d186ca9dc62f91a
tarun2506 <tarunsharma8920@gmail.com>
2021-09-01 18:24:35 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 function App() {
   return (
     <div className="App">
       {/* Navbar */}
+      <Nav />
       <Banner />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./nav.css";
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+      <img
+        className="nav-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav-avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
